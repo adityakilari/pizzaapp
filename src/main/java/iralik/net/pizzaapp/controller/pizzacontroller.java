4c816c6cb6b1b0ae7ec024b95bae0925f835e84a
sivateja <sivatejakolasani@gmail.com>
2023-09-25 16:33:34 -0400

    post method created

--- src/main/java/iralik/net/pizzaapp/controller/pizzacontroller.java
+++ src/main/java/iralik/net/pizzaapp/controller/pizzacontroller.java
@@ -2,6 +2,7 @@ package iralik.net.pizzaapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class pizzacontroller {
@@ -15,6 +16,8 @@ public class pizzacontroller {
         System.out.println("My topping");
     }
 
-
-
+    @PostMapping("/myorder")
+    public void createOrder(){
+            System.out.println("I need Small pizza");
+    }
 }
